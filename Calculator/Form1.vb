31ed1f46352197c0c319c3337773bb5d97da4d89
SdHinokamiKagura <satyad123.git.hub.in@gmail.com>
2023-12-18 21:48:26 +0530

    Add project files.

---
+++ Calculator/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim firstNum, secondNum, Result As Double
+        If TextBox1.Text <> "" And TextBox2.Text <> "" Then
+            firstNum = CDbl(TextBox1.Text)
+            secondNum = CDbl(TextBox2.Text)
+
+            Result = firstNum + secondNum
+            TextResult.Text = "The Result of " & firstNum & " and " & secondNum & " is :" & Result
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim firstNum, secondNum, Result As Double
+        If TextBox1.Text <> "" And TextBox2.Text <> "" Then
+            firstNum = CDbl(TextBox1.Text)
+            secondNum = CDbl(TextBox2.Text)
+
+            Result = firstNum - secondNum
+            TextResult.Text = "The Result of " & firstNum & " and " & secondNum & " is :" & Result
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim firstNum, secondNum, Result As Double
+        If TextBox1.Text <> "" And TextBox2.Text <> "" Then
+            firstNum = CDbl(TextBox1.Text)
+            secondNum = CDbl(TextBox2.Text)
+
+            Result = firstNum * secondNum
+            TextResult.Text = "The Result of " & firstNum & " and " & secondNum & " is :" & Result
+        End If
+    End Sub
+End Class
